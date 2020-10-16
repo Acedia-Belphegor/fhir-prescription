61c82643d70eabcf36038ae14ca42ab3ef3f33ed
yoshinori.kodama <yoshinori.kodama@C01-02-18-0024.local>
2020-10-16 21:56:31 +0900

    fix

--- lib/from_cda/cda_fhir_abstract_generator.rb
+++ lib/from_cda/cda_fhir_abstract_generator.rb
@@ -17,6 +17,7 @@ class CdaFhirAbstractGenerator
         @client.use_r4
         FHIR::Model.client = @client            
         @bundle = FHIR::Bundle.new
+        @bundle.id = SecureRandom.uuid
         @bundle.type = :document
     end
 
--- lib/from_qr/qr_generate_practitioner.rb
+++ lib/from_qr/qr_generate_practitioner.rb
@@ -25,7 +25,7 @@ class QrGeneratePractitioner < QrGenerateAbstract
             end
             extension = FHIR::Extension.new
             extension.url = "http://hl7.org/fhir/StructureDefinition/iso21090-EN-representation"
-            extension.valueCode = 'I' # 漢字
+            extension.valueCode = :IDE # 漢字
             human_name.extension << extension
             practitioner.name << human_name
         end
@@ -43,7 +43,7 @@ class QrGeneratePractitioner < QrGenerateAbstract
             end
             extension = FHIR::Extension.new
             extension.url = "http://hl7.org/fhir/StructureDefinition/iso21090-EN-representation"
-            extension.valueCode = 'P' # カナ
+            extension.valueCode = :SYL # カナ
             human_name.extension << extension
             practitioner.name << human_name
         end
